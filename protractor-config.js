128fc7294085c7cbedf1a4d6d884c23daa08844e
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-08-28 19:28:38 +0300

    lil

--- protractor-config.js
+++ protractor-config.js
@@ -1,7 +1,5 @@
 exports.config = {
 //  directConnect: true,
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
   specs: ['test/e2e/profile/*.js'],
   baseUrl: 'http://localhost',
   allScriptsTimeout: 40000,
@@ -9,9 +7,5 @@ exports.config = {
   framework: 'mocha',
   mochaOpts: {
     reporter: 'spec'
-  },
-  capabilities: {
-    'browserName': 'firefox',
-    'build': process.env.TRAVIS_BUILD_NUMBER
   }
 };
--- test/e2e/profile/test_spec.js
+++ test/e2e/profile/test_spec.js
@@ -6,9 +6,26 @@ var expect = chai.expect;
 
 describe('Test', function() {
   this.timeout(15000);
-  
+
   beforeEach(function() {
-    browser.get('http://localhost:9000/#/profile');
+    if (process.env.SAUCE_USERNAME != undefined) {
+      this.browser = new webdriver.Builder()
+      .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
+      .withCapabilities({
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        username: process.env.SAUCE_USERNAME,
+        accessKey: process.env.SAUCE_ACCESS_KEY,
+        browserName: "chrome"
+      }).build();
+    } else {
+      this.browser = new webdriver.Builder()
+      .withCapabilities({
+        browserName: "chrome"
+      }).build();
+    }
+
+    return this.browser.get("http://localhost:9000/#/profile");
   });
 
   it('should have email there', function() {
