69df627cd9c0b17a7e224b8c0d9cf0930429d82f
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-08-28 18:51:09 +0300

    next one

--- protractor-config.js
+++ protractor-config.js
@@ -3,14 +3,15 @@ exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   specs: ['test/e2e/profile/*.js'],
-  baseUrl: 'http://localhost:9000/#',
+  baseUrl: 'http://localhost',
+  allScriptsTimeout: 40000,
+  getPageTimeout: 40000,
   framework: 'mocha',
   mochaOpts: {
-    reporter: 'spec',
-    slow: 3000,
-    tmeout: 3000
+    reporter: 'spec'
   },
   capabilities: {
-    'browserName': 'firefox'
+    'browserName': 'firefox',
+    'build': process.env.TRAVIS_BUILD_NUMBER
   }
 };
