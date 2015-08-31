6742b690ac67937feade0b0cb6f2e57fbdeff3e5
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-08-31 15:58:09 +0300

    another

--- protractor-config.js
+++ protractor-config.js
@@ -7,16 +7,5 @@ var config ={
     enableTimeouts: false
   }
 };
-
-if (process.env.TRAVIS_BUILD_NUMBER) {
-  config.sauceUser = process.env.SAUCE_USERNAME;
-  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-  config.capabilities = {
-    'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Test'
-  };
-}
  
 exports.config = config;
