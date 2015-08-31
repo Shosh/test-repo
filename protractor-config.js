ec5091b3319cf1c54ecea4d390495d72e3b3586b
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-08-31 15:21:28 +0300

    next

--- protractor-config.js
+++ protractor-config.js
@@ -1,21 +1,25 @@
-exports.config = {
-//  directConnect: true,
-  specs: ['test/e2e/profile/*.js'],
-  baseUrl: 'http://localhost:9000/#',
-  allScriptsTimeout: 40000,
-  getPageTimeout: 40000,
-  framework: 'mocha',
-  mochaOpts: {
-    reporter: 'spec'
-  }  
+var config ={
+  specs:['test/e2e/profile/*.js'],
+  framework:'mocha',
+  mochaOpts:{
+    reporter:'spec',
+    slow:3000,
+    enableTimeouts: false
+  },
+  capabilities:{
+    'browserName':'chrome'
+  }
 };
 
-if (process.env.TRAVIS) {
+if (process.env.TRAVIS_BUILD_NUMBER) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
   config.capabilities = {
     'browserName': 'chrome',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Test'
   };
 }
+ 
+exports.config = config;
